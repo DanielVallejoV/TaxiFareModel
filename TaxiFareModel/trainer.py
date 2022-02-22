f4ff02dc264b3cf4abc8b0c8ef3c5aa0d3275474
Daniel Vallejo <d.vallejovial@gmail.com>
2022-02-22 15:52:18 +0000

    MLflow quickstart completed

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -6,6 +6,11 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
+from TaxiFareModel.data import get_data, clean_data
+from sklearn.model_selection import train_test_split
 
 class Trainer():
     def __init__(self, X, y):
@@ -16,6 +21,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = "[UK][London][DanielVallejoV] Taxifare 1"
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -61,11 +67,47 @@ class Trainer():
         return score
 
 
+    @memoized_property
+    def mlflow_client(self):
+        MLFLOW_URI = "https://mlflow.lewagon.co/"
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
 if __name__ == "__main__":
     # get data
+    df = get_data()
     # clean data
+    df_cleaned = clean_data(df)
     # set X and y
+    y = df["fare_amount"]
+    X = df.drop("fare_amount", axis=1)
     # hold out
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
     # train
+    trainer = Trainer(X_train, y_train)
+    trainer.run()
     # evaluate
+    print(trainer.evaluate(X_val, y_val))
+
+
+    trainer.mlflow_log_metric("rmse", trainer.evaluate(X_val, y_val))
+    trainer.mlflow_log_param("model", 'LinearRegression()')
     print('TODO')
